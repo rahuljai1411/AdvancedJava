ed82022f5059d1241e57da915b81b5a530c1a663
rahuljai1411 <rahulroxx311623@gmail.com>
2024-09-05 14:06:45 +0530

    Add files via upload

---
+++ Prac_1B/src/java/Myservlet1.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Myservlet1 extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Myservlet1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            String S1=request.getParameter("name");
+            String P1=request.getParameter("pass");
+            
+            if(S1.equals("Admin")&&P1.equals("upg"))
+            {
+            out.println("<H1>Login Successful</H1>");
+            out.println("Name: "+S1);
+            out.println("Password: "+P1);
+            }
+            else{
+            out.println("Login failed");
+            }
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+  
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
