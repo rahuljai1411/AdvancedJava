fe782edabba388aa9d6c7709d83100af49858861
rahuljai1411 <rahulroxx311623@gmail.com>
2024-09-05 14:12:52 +0530

    Add files via upload

---
+++ Prac_2A/src/java/LoginServlet.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+
+           
+                if ("ADMIN".equals(username) && "Servlet".equals(password)) {
+                    request.setAttribute("user", username);
+                    RequestDispatcher rd = request.getRequestDispatcher("WelcomeServlet");
+                    rd.forward(request, response);
+                } else {
+                    out.println("<H1>Login Fail!!!</H1>");
+                    out.println("<body style=\"background-color:red;\">");
+                    RequestDispatcher rd = request.getRequestDispatcher("index.html");
+                    rd.include(request, response);
+                }
+                
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ Prac_2A/src/java/WelcomeServlet.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WelcomeServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String username = (String) request.getAttribute("user");
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Welcome</title>");
+            out.println("</head>");
+            out.println("<body style=\"background-color:green;\">");
+            out.println("Welcome, " + username + "!");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
